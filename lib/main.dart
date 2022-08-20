2b87b27651b50d9f48ac7fd6832bf45f8b2ee0fc
setiyonore <setiyono.ressly@gmail.com>
2022-08-20 18:28:41 +0700

    add theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bank_sha/shared/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -78,8 +79,11 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Image.asset(
-          'assets/img_logo_light.png',
+        child: Text(
+          'Hello semua',
+          style: blueTextStyle.copyWith(
+            fontSize: 30,
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
