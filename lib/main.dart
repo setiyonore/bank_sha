bdc55a44e0383595921bc90bc446333317c039fc
setiyonore <setiyono.ressly@gmail.com>
2022-08-22 07:34:19 +0700

    add onboarding page

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: splashPage(),
+      home: SplashPage(),
     );
   }
 }
--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -1,8 +1,30 @@
+import 'dart:async';
+
 import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/pages/onboarding_page.dart';
 import 'package:flutter/material.dart';
 
-class splashPage extends StatelessWidget {
-  const splashPage({Key? key}) : super(key: key);
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const OnboardingPage(),
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
