86f669ab9cc74a804b5a4081ab36b7c3fa4b91da
setiyonore <setiyono.ressly@gmail.com>
2022-08-27 14:30:41 +0700

    add tips item

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/widgets/home_service_item.dart';
+import 'package:bank_sha/ui/widgets/home_tips_item.dart';
 import 'package:bank_sha/ui/widgets/home_transaction_latest_item.dart';
 import 'package:bank_sha/ui/widgets/home_user_item.dart';
 import 'package:flutter/material.dart';
@@ -428,6 +429,7 @@ class HomePage extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.only(
         top: 30,
+        bottom: 50,
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -441,6 +443,30 @@ class HomePage extends StatelessWidget {
           ),
           const SizedBox(
             height: 14,
+          ),
+          Wrap(
+            direction: Axis.horizontal,
+            runAlignment: WrapAlignment.start,
+            spacing: 17,
+            runSpacing: 18,
+            children: const [
+              HomeTipsItem(
+                  imageUrl: 'assets/img_tips1.png',
+                  title: 'Best tips for using\na credit card',
+                  url: 'https://www.google.com'),
+              HomeTipsItem(
+                  imageUrl: 'assets/img_tips2.png',
+                  title: 'Spot the good pie\nof finance model',
+                  url: 'https://www.google.com'),
+              HomeTipsItem(
+                  imageUrl: 'assets/img_tips3.png',
+                  title: 'Great hack to get\nbetter advices',
+                  url: 'https://www.google.com'),
+              HomeTipsItem(
+                  imageUrl: 'assets/img_tips4.png',
+                  title: 'Save more penny\nbuy this instead',
+                  url: 'https://www.google.com')
+            ],
           ),
         ],
       ),
