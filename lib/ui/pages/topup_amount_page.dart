4f8ae402d1ec12096bacca27fa6770217cb8683f
setiyonore <setiyono.ressly@gmail.com>
2022-09-02 10:12:12 +0700

    launch url midtrans and add number formatting

--- lib/ui/pages/topup_amount_page.dart
+++ lib/ui/pages/topup_amount_page.dart
@@ -1,5 +1,7 @@
 import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/widgets/buttons.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 
 class TopupAmountPage extends StatefulWidget {
@@ -12,6 +14,25 @@ class TopupAmountPage extends StatefulWidget {
 class _TopupAmountPageState extends State<TopupAmountPage> {
   final TextEditingController amountController =
       TextEditingController(text: '0');
+
+  @override
+  void initState() {
+    super.initState();
+    amountController.addListener(() {
+      final text = amountController.text;
+      amountController.value = amountController.value.copyWith(
+        text: NumberFormat.currency(
+          locale: 'id',
+          decimalDigits: 0,
+          symbol: '',
+        ).format(
+          int.parse(
+            text.replaceAll('.', ''),
+          ),
+        ),
+      );
+    });
+  }
 
   addAmount(String number) {
     if (amountController.text == '0') {
@@ -193,6 +214,7 @@ class _TopupAmountPageState extends State<TopupAmountPage> {
             title: 'Topup Now',
             onPressed: () async {
               if (await Navigator.pushNamed(context, '/pin') == true) {
+                await launch('https://demo.midtrans.com/');
                 Navigator.pushNamedAndRemoveUntil(
                     context, '/topup-success', (route) => false);
               }
