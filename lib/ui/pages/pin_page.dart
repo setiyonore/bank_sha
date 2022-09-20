af5468b2413e5e95e11c5cb01a087234b4b286fe
setiyonore <setiyono.ressly@gmail.com>
2022-09-20 19:27:10 +0700

    change pin to dinamic

--- lib/ui/pages/pin_page.dart
+++ lib/ui/pages/pin_page.dart
@@ -1,7 +1,11 @@
+import 'dart:math';
+
+import 'package:bank_sha/blocs/auth/auth_bloc.dart';
 import 'package:bank_sha/shared/shared_method.dart';
 import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/widgets/buttons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PinPage extends StatefulWidget {
   const PinPage({Key? key}) : super(key: key);
@@ -12,6 +16,8 @@ class PinPage extends StatefulWidget {
 
 class _PinPageState extends State<PinPage> {
   final TextEditingController pinController = TextEditingController(text: '');
+  String pin = '';
+  bool isError = false;
 
   addPin(String number) {
     if (pinController.text.length < 6) {
@@ -19,25 +25,37 @@ class _PinPageState extends State<PinPage> {
         pinController.text = pinController.text + number;
       });
     }
-    if(pinController.text.length == 6){
-      if (pinController.text == '123123') {
+    if (pinController.text.length == 6) {
+      if (pinController.text == pin) {
         Navigator.pop(context, true);
       } else {
+        setState(() {
+          isError = true;
+        });
         showCustomSnackbar(
             context, 'PIN yang anda masukkan salah. Silakan coba lagi.');
       }
     }
-
   }
 
   deletePin() {
     if (pinController.text.isNotEmpty) {
       setState(() {
+        isError = false;
         pinController.text = pinController.text.substring(
           0,
           pinController.text.length - 1,
         );
       });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    final authState = context.read<AuthBloc>().state;
+    if (authState is AuthSuccess) {
+      pin = authState.user.pin!;
     }
   }
 
@@ -72,7 +90,11 @@ class _PinPageState extends State<PinPage> {
                   obscuringCharacter: '*',
                   enabled: false,
                   style: whiteTextStyle.copyWith(
-                      fontSize: 36, fontWeight: medium, letterSpacing: 16),
+                    fontSize: 36,
+                    fontWeight: medium,
+                    letterSpacing: 16,
+                    color: isError ? redColor : whiteColor,
+                  ),
                   decoration: InputDecoration(
                     disabledBorder: UnderlineInputBorder(
                       borderSide: BorderSide(
