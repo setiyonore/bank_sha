dd5fc49996e17462837f5a5141a17a2558fde234
setiyonore <setiyono.ressly@gmail.com>
2022-09-20 19:26:17 +0700

    change pin to dinamic

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,61 +1,108 @@
 class UserModel {
-  final int? id;
-  final String? name;
-  final String? email;
-  final String? username;
-  final String? password;
-  final int? verified;
-  final String? profilePicture;
-  final int? balance;
-  final String? cardNumber;
-  final String? pin;
-  final String? token;
+  int? id;
+  String? name;
+  String? email;
+  String? username;
+  String? password;
+  String? emailVerifiedAt;
+  int? verified;
+  String? profilePicture;
+  String? ktp;
+  String? createdAt;
+  String? updatedAt;
+  int? balance;
+  String? cardNumber;
+  String? pin;
+  String? token;
+  int? tokenExpiresIn;
+  String? tokenType;
 
-  UserModel({
-    this.id,
-    this.name,
-    this.email,
-    this.username,
-    this.password,
-    this.verified,
-    this.profilePicture,
-    this.balance,
-    this.cardNumber,
-    this.pin,
-    this.token,
-  });
+  UserModel(
+      {this.id,
+        this.name,
+        this.email,
+        this.username,
+        this.password,
+        this.emailVerifiedAt,
+        this.verified,
+        this.profilePicture,
+        this.ktp,
+        this.createdAt,
+        this.updatedAt,
+        this.balance,
+        this.cardNumber,
+        this.pin,
+        this.token,
+        this.tokenExpiresIn,
+        this.tokenType});
 
-  factory UserModel.fromJson(Map<String,dynamic>json) =>
-      UserModel(
-        id: json['id'],
-        name: json['name'],
-        email: json['email'],
-        username: json['username'],
-        verified: json['verified'],
-        profilePicture: json['profile_picture'],
-        balance: json['balance'],
-        cardNumber: json['card_number'],
-        pin: json['pin'],
-        token: json['token'],
-      );
+  UserModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    email = json['email'];
+    username = json['username'];
+    password = json['password'];
+    emailVerifiedAt = json['email_verified_at'];
+    verified = json['verified'];
+    profilePicture = json['profile_picture'];
+    ktp = json['ktp'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+    balance = json['balance'];
+    cardNumber = json['card_number'];
+    pin = json['pin'];
+    token = json['token'];
+    tokenExpiresIn = json['token_expires_in'];
+    tokenType = json['token_type'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['email'] = email;
+    data['password'] = password;
+    data['username'] = username;
+    data['email_verified_at'] = emailVerifiedAt;
+    data['verified'] = verified;
+    data['profile_picture'] = profilePicture;
+    data['ktp'] = ktp;
+    data['created_at'] = createdAt;
+    data['updated_at'] = updatedAt;
+    data['balance'] = balance;
+    data['card_number'] = cardNumber;
+    data['pin'] = pin;
+    data['token'] = token;
+    data['token_expires_in'] = tokenExpiresIn;
+    data['token_type'] = tokenType;
+    return data;
+  }
+
   UserModel copyWith({
-  String? username,
-  String? name,
-  String? email,
-  String? pin,
-  String? password,
-  int? balance,
-
-})=>UserModel(
-    id: id,
-    username: username ?? this.username,
-    name: name ?? this.name,
-    pin: pin ?? this.pin,
-    password: password ?? this.password,
-    balance: balance ?? this.balance,
-    verified: verified,
-    profilePicture: profilePicture,
-    cardNumber: cardNumber,
-    token: token,
-  );
+    String? username,
+    String? name,
+    String? email,
+    String? pin,
+    String? password,
+    int? balance,
+  }) =>
+      UserModel(
+        id: id,
+        username: username ?? this.username,
+        name: name ?? this.name,
+        email: email ?? this.email,
+        password: password ?? this.password,
+        emailVerifiedAt: emailVerifiedAt,
+        verified: verified,
+        profilePicture: profilePicture,
+        ktp: ktp,
+        createdAt: createdAt,
+        updatedAt: updatedAt,
+        balance: balance ?? this.balance,
+        cardNumber: cardNumber,
+        pin: pin ?? this.pin,
+        token: token,
+        tokenExpiresIn: tokenExpiresIn,
+        tokenType: tokenType,
+      );
 }
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -4,6 +4,7 @@ import 'package:bank_sha/models/sign_in_form_model.dart';
 import 'package:bank_sha/models/sign_up_form_model.dart';
 import 'package:bank_sha/models/user_model.dart';
 import 'package:bank_sha/shared/shared_values.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService {
@@ -36,7 +37,8 @@ class AuthService {
         UserModel user = UserModel.fromJson(
           jsonDecode(res.body),
         );
-        user.copyWith(password: data.password);
+        user.password = data.password;
+        await storeCredentialToLocal(user);
         return user;
       } else {
         throw jsonDecode(res.body)['message'];
@@ -45,6 +47,7 @@ class AuthService {
       rethrow;
     }
   }
+
   Future<UserModel> login(SignInFormModel data) async {
     try {
       final res = await http.post(
@@ -55,7 +58,10 @@ class AuthService {
         UserModel user = UserModel.fromJson(
           jsonDecode(res.body),
         );
-        user.copyWith(password: data.password);
+        print(user.password);
+        user.password = data.password;
+        await storeCredentialToLocal(user);
+        print(user.password);
         return user;
       } else {
         throw jsonDecode(res.body)['message'];
@@ -63,5 +69,48 @@ class AuthService {
     } catch (e) {
       rethrow;
     }
+  }
+
+  Future<void> storeCredentialToLocal(UserModel user) async {
+    try {
+      const storage = FlutterSecureStorage();
+      await storage.write(key: 'token', value: user.token);
+      await storage.write(key: 'email', value: user.email);
+      await storage.write(key: 'password', value: user.password);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<SignInFormModel> getCredentialFromLocal() async {
+    try {
+      const storage = FlutterSecureStorage();
+      Map<String, String> values = await storage.readAll();
+      if (values['email'] == null || values['password'] == null) {
+        throw 'authenticated';
+      } else {
+        final SignInFormModel data = SignInFormModel(
+          email: values['email'],
+          password: values['password'],
+        );
+        return data;
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+  Future<String> getToken() async{
+    String token = '';
+    const storage = FlutterSecureStorage();
+    String? value = await storage.read(key: 'token');
+    if(value != null){
+      token = 'Bearer '+value;
+    }
+    return token;
+  }
+
+  Future<void> clearLocalStorage() async{
+    const storage = FlutterSecureStorage();
+    await storage.deleteAll();
   }
 }
